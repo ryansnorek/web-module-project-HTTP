1df0bc273a48f0f3f8cebb32e9aa538766402f4f
Ryan <snorekr@gmail.com>
2021-11-10 16:55:40 -0500

    add movie

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import MovieHeader from './components/MovieHeader';
 
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from "./components/AddMovieForm";
 
 import axios from 'axios';
 
@@ -45,6 +46,10 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies}/>
         
           <Switch>
+            <Route path="/movies/add">
+              <AddMovieForm movies={movies} setMovies={setMovies}/>
+            </Route>
+
             <Route path="/movies/edit/:id">
               <EditMovieForm movies={movies} setMovies={setMovies}/>
             </Route>
